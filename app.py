1c3a6dcae77ba4894208b54b4ccf9b44b701af72
Tianyang Li <501601709@qq.com>
2023-03-13 13:48:42 +0000

    update app.py

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def input_value():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=app.config["DEBUG"], threaded=False)
+    app.run(host='0.0.0.0', port=5000, debug=app.config["DEBUG"], threaded=False)
