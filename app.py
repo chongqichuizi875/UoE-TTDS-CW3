f3b3bfca12825a276b0cccc6f5b9d8142fbb4d86
Tianyang Li <501601709@qq.com>
2023-03-14 16:34:51 +0000

    update frontend and app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 CORS().init_app(app)
 
-url_search_results = 'http://127.0.0.1:5000/search/'
-url_wiki_introduce = 'http://127.0.0.1:5000/wiki/'
+web_url = 'https://en.wikipedia.org/'
 
 
 @app.route('/')
@@ -28,6 +27,9 @@ def wiki_introduce(name):
     后端-->前端：
     (1) title: 词条title，需转化为字符串
     (2) introduce: 词条具体介绍，需转化为字符串
+    (3) web_url: 提前设定好的，后端提前设定好web_url的值为'https://en.wikipedia.org/'，
+    "wiki.html"这个html文件中已设定好前端输出格式为web_url+"wiki/"+后端向前端传输的title，
+    比如Association football，前端输出格式就为"https://en.wikipedia.org/wiki/Association football"
     """
     if request.method == 'GET':
         title = str(name)
@@ -37,9 +39,7 @@ def wiki_introduce(name):
         db_session.close()
         query_title = infos.title  # 词条title
         query_introduce = infos.introduce  # 词条介绍
-        # query_url = infos.url
-        # return render_template('wiki.html', title=title, introduce=introduce, url=url)
-        return render_template('wiki.html', title=str(query_title), introduce=str(query_introduce))
+        return render_template('wiki.html', title=str(query_title), introduce=str(query_introduce), web_url=web_url)
 
 
 # 搜索结果界面
@@ -51,11 +51,10 @@ def search_results(name):
     (1) title=str(name): 词条title,需将其转化为字符串
     (2) page_id: 作为分页id，比如100条记录，每页存储10条记录，第1页id就是1;当前端转到第2页, id自动变成2
     后端-->前端
-    (1) infos_list=[{'title': title1, 'introduce':introduce1, 'url': url1},
-                    {'title': title2, 'introduce':introduce2, 'url': url2},
+    (1) infos_list=[{'title': title1, 'introduce':introduce1},
+                    {'title': title2, 'introduce':introduce2},
                     ...]: 包含所有搜索结果的列表形式，每一个搜索结果用字典储存
-        字典里每一项按顺序分别为词条title，词条introduce和词条url
-        url格式为'http://127.0.0.1:5000/wiki/'+词条title，在前端点击链接后转到词条具体介绍界面
+        字典里每一项按顺序分别为词条title和词条introduce
     (2) len_number: 搜索结果的数量
     """
     # GET操作，则显示搜索结果界面
@@ -73,8 +72,7 @@ def search_results(name):
         for i in infos:
             # 不管是大写小写都能搜索出结果
             if title.lower() in str(i.title).lower() or title.upper() in str(i.title).upper():
-                infos_list.append({'title': i.title, 'introduce': i.introduce[0:200] + '...',
-                                   'url': url_wiki_introduce + i.title})
+                infos_list.append({'title': i.title, 'introduce': i.introduce[0:200] + '...'})
         len_number = int(len(infos_list))
         # 第1页就是放搜索结果[0:10], 第2页[11:20]，以此类推
         infos_list = infos_list[(page_id * 10 - 10):(page_id * 10)]
@@ -88,13 +86,10 @@ def input_value():
     前端-->后端
     (1) input_value: 在输入框中输入的query
     后端-->前端
-    (1) infos_list=[{'title': title1, 'introduce':introduce1, 'url': url1},
-                    {'title': title2, 'introduce':introduce2, 'url': url2},
+    (1) infos_list=[{'title': title1, 'introduce':introduce1},
+                    {'title': title2, 'introduce':introduce2},
                     ...]: 包含前10条模糊搜索的列表形式，每一个搜索结果用字典储存
-        字典里每一项按顺序分别为词条title，词条introduce和词条url
-        url格式为'http://127.0.0.1:5000/search/'+词条title，在前端点击链接后转到搜索结果界面
-    这部分考虑修改为，点击模糊搜索提示词，不是转到具体介绍界面，而是将模糊搜索词填充到输入框，然后点击搜索，跳转到搜索结果页面
-    :return:
+        字典里每一项按顺序分别为词条title和词条introduce
     """
     if request.method == 'POST':
         input_value = str(request.get_json()['input_value'])
@@ -106,9 +101,8 @@ def input_value():
         # e.g. 假如输入框输入ja，则直接从整个wikipedia的词条数据库中获得所有带ja的词条，并选取前10个显示在模糊提示框内
         for i in infos:
             if input_value.lower() in i.title.lower() or input_value.upper() in i.title.upper():
-                infos_list.append({'title': i.title, 'introduce': i.introduce, 'url': url_search_results + i.title})
+                infos_list.append({'title': i.title, 'introduce': i.introduce})
         infos_list = infos_list[0:10]
-
         return jsonify(infos_list)
 
 
